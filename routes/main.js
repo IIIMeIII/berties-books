352d2eded742762337f5bf97760e3419b1b1eed2
tclar <tclar001@campus.goldsmiths.ac.uk>
2023-11-22 12:26:05 +0000

    You got up to step 14 in Lab 3 part 2

--- routes/main.js
+++ routes/main.js
@@ -22,6 +22,11 @@ module.exports = function(app, shopData) {
         res.render('register.ejs', shopData);                                                                     
     }); 
 
+    app.get('/addbook', function (req,res) {
+        res.render('addbook.ejs', shopData);                                                                     
+    }); 
+
+
     app.get('/list', function(req, res) {
         let sqlquery = "SELECT * FROM books"; // query database to get all the books
         // execute sql query
@@ -29,12 +34,32 @@ module.exports = function(app, shopData) {
             if (err) {
                 res.redirect('./'); 
             }
-            res.send(result)
+
+            let newData = Object.assign({}, shopData, {availableBooks:result});
+            console.log(newData);
+            res.render("list.ejs", newData);
+
          });
     });
-
+ 
     app.post('/registered', function (req,res) {
         // saving data in database
         res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
     }); 
+
+    app.post('/bookadded', function (req,res) {
+        // saving data in database
+        let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+          if (err) {
+            return console.error(err.message);
+          }
+          else {
+            res.send(' This book is added to database, name: '
+                      + req.body.name + ' price '+ req.body.price);
+          }
+        });
+    });
 }
